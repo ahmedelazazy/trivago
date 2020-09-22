2f2da9c28ec1f9c44281233ef38a87aa399fef62
Ahmed Elazazy <ahmed.elazazy@hotmail.com>
2020-09-22 21:46:51 +0200

    Update seeds to use Faker

--- seeders/20200918162246-demo-hotelier.js
+++ seeders/20200918162246-demo-hotelier.js
@@ -1,40 +1,49 @@
+const { randomHoterlier, randomShalowHoterlierItem, randomLocation } = require("../tests/helper");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("hoteliers", [
-      {
-        name: "Hilton",
+    let hoteliers = [];
+    let locations = [];
+    let hotelierItems = [];
+
+    for (let i = 0; i < 10; i++) {
+      hoteliers.push({
+        ...randomHoterlier(),
         createdAt: new Date(),
         updatedAt: new Date(),
-      },
-      {
-        name: "Mariot",
+      });
+    }
+
+    let hoteliersIds = await queryInterface.bulkInsert("hoteliers", hoteliers, { returning: ["id"] });
+
+    for (let i = 0; i < 100; i++) {
+      locations.push({
+        ...randomLocation(),
         createdAt: new Date(),
         updatedAt: new Date(),
-      },
-      {
-        name: "Ibis",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "Holiday Inn",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "Radisson",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: "InterContinental",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+      });
+    }
+
+    let locationsIds = await queryInterface.bulkInsert("locations", locations, { returning: ["id"] });
+
+    for (let i = 0; i < hoteliersIds.length; i++) {
+      for (let j = 0; j < 10; j++) {
+        hotelierItems.push({
+          ...randomShalowHoterlierItem(),
+          hotelierId: hoteliersIds[i].id,
+          locationId: locationsIds[j + i * hoteliersIds.length].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        });
+      }
+    }
+
+    await queryInterface.bulkInsert("hotelier_items", hotelierItems, { returning: ["id"] });
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete("hoteliers", null, {});
+    await queryInterface.bulkDelete("hotelier_items", null, {});
+    await queryInterface.bulkDelete("locations", null, {});
+    await queryInterface.bulkDelete("hoteliers", null, {});
   },
 };
