8de14f745c3a97dba9a2b72070a44bcb43d6b9e5
Ahmed Elazazy <ahmed.elazazy@hotmail.com>
2020-09-22 13:54:24 +0200

    Add publishing public static folder middleware

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(morgan);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static("public"));
 
 app.use("/", indexRouter);
 
